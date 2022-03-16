29a981033f24bed06bdc25d357cca39b1996b1bf
zogak <zogak7@gmail.com>
2022-03-16 19:23:43 +0900

    init

---
+++ vogel/src/main/java/com/teamSix/vogel/VogelApplication.java
@@ -0,0 +1,13 @@
+package com.teamSix.vogel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VogelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VogelApplication.class, args);
+	}
+
+}
---
+++ vogel/src/test/java/com/teamSix/vogel/VogelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamSix.vogel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VogelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
